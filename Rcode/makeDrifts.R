08130ee92f73ccc4f86bfb3c566ce5e45bbee679
bdoll <bradleybdoll@gmail.com>
2015-03-24 09:15:58 -0400

    better makeDrifts

--- Rcode/makeDrifts.R
+++ Rcode/makeDrifts.R
@@ -2,32 +2,40 @@ makeDrifts <- function(write=FALSE, plotDrifts=FALSE) {
 # Make new reward probability drifts with reflecting boundaries of
 # 0.25 and 0.75. arg write == true overwrites existing drift
 # file. plotDrifts shows the random walk of probabilities over 
-# trials for the two task choices.
+# trials for the two task choices. makeDrifts only generates new
+# drifts if write == true (permits plotting of existing drifts in payProbDrift.csv)
 
+if (write) {
+
+    totaltrials <- 200
     payoff <- matrix(0, totaltrials, 2)
     payoff[1,] <- runif(2, 0.25, 0.75)
 
-for (i in 2:totaltrials) {
+    for (i in 2:totaltrials) {
 
-    prob <- payoff[i-1, ] + rnorm(2, mean=0, sd = 0.025)
+        prob <- payoff[i-1, ] + rnorm(2, mean=0, sd = 0.025)
 
-    prob[which(prob > 0.75)] <- 1.5 - prob[which(prob > 0.75)]
-    prob[which(prob < 0.25)] <-  .5 - prob[which(prob < 0.25)]
+        prob[which(prob > 0.75)] <- 1.5 - prob[which(prob > 0.75)]
+        prob[which(prob < 0.25)] <-  .5 - prob[which(prob < 0.25)]
     
-    payoff[i,] <- prob
+        payoff[i,] <- prob
         
     }
-
-    if (write) {
-        write.table(payoff, "payProbDrift1.csv", row.names=F,
-                    col.names=F, quote=F, sep=",")
-    }
-
-    if (plotDrifts) {
-        plot(payoff[,1], type="l", ylim=c(0.25, 0.75), ylab="prob", xlab="trial")
-        lines(payoff[,2], lty=2)
-    }
     
+        write.table(payoff, "payProbDrift.csv", row.names=F,
+                    col.names=F, quote=F, sep=",")
+    
+} else {
+
+       payoff <- read.table("payProbDrift.csv", header=F, sep=",")    
+
+}
+
+if (plotDrifts) {
+        plot(payoff[,1], type="l", ylim=c(0.25, 0.75), ylab="probability", xlab="trial")
+        lines(payoff[,2], lty=2)
+} 
+
 return(payoff)
 
 }
